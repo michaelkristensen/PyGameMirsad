ff79530e36d24c18f113ca29d724d13695ea6851
Lasse Hyldahl Jensen <lasse@lasse-it.dk>
2017-10-10 13:58:13 +0200

    Done with mainFile

--- mainFile.py
+++ mainFile.py
@@ -1,10 +1,31 @@
-import trekantsFunktioner
+import pygame.locals, trekantsFunktioner
 
-x1 = input("x1: ")
-x2 = input("x2: ")
-x3 = input("x3: ")
-y1 = input("y1: ")
-y2 = input("y2: ")
-y3 = input("y3: ")
 
-trekantsFunktioner.findTrekantSidelaengder(x1,x2,x3,y1,y2,y3)
+pygame.init()
+
+w = 1000
+h = 800
+isRunning = True
+corners = [];
+
+window = pygame.display.set_mode([w,h])
+window.fill([255,255,255])
+
+while isRunning:
+    for event in pygame.event.get():
+        if event.type == pygame.locals.MOUSEBUTTONUP:
+            if len(corners) == 3:
+                corners.clear()
+                window.fill([255, 255, 255])
+            corners.append(pygame.mouse.get_pos())
+        elif event.type == pygame.locals.QUIT:
+            exit()
+
+    for corner in corners:
+        x,y = corner
+        pygame.draw.circle(window,[255,0,0],[x,y],5)
+
+    if len(corners) == 3:
+        pygame.draw.polygon(window,[0,0,255],corners,2)
+
+    pygame.display.update()
