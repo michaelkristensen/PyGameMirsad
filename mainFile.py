c0ad1952621f23c3f30109947fbe7f62ef8a4a60
mka <mka@aarhustech.dk>
2017-10-10 11:34:19 +0200

    Funktioner

---
+++ mainFile.py
@@ -0,0 +1,10 @@
+import trekantsFunktioner
+
+x1 = input("x1: ")
+x2 = input("x2: ")
+x3 = input("x3: ")
+y1 = input("y1: ")
+y2 = input("y2: ")
+y3 = input("y3: ")
+
+trekantsFunktioner.findTrekantSidelaengder(x1,x2,x3,y1,y2,y3)
---
+++ trekantsFunktioner.py
@@ -0,0 +1,34 @@
+import math
+def findTrekantOmkreds(): #Made by Yusuf, Hadi
+    return 0
+
+def findTrekantSidelaengder(x1,x2,x3,y1,y2,y3): #Made by Bjørn, Rasmus E, Nick
+    a = math.sqrt((x1-x2)**2+(y1-y2)**2)
+    return [a,b,c]
+
+def findTrekantAreal(): #Made by Jakob, Rasmus W
+    return 0
+
+def findTrekantVinkler(): #Made by Said, Omar
+    return 0
+
+def findTrekantOmskrevneCirkelsRadius(): #Made by Gwion, Frederik L
+    return 0
+
+def findTrekantOmskrevneCirkelsKordinater(): #Made by Oliver, Mads, Liam
+    return 0
+
+def findTrekantIndskrevneCirkelsRadius(): #Made by Michael, Alexander
+    return 0
+
+def findTrekantIndskrevneCirkelsKordinater(): #Made by Allan, Gabriel, Daniel
+    return 0
+
+def findTrekantVinkelHalveringsLinjer(): #Made by
+    return 0
+
+def tegnTrekant(): #Made by Yusuf, Hadi
+    return 0
+
+def skrivResultater():
+    return 0
