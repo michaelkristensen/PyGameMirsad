0588bc119764dfe9e6c627e2657a4a3c5e78ba0e
mka <mka@aarhustech.dk>
2017-10-24 11:22:40 +0200

    Funktioner

---
+++ Video.py
@@ -0,0 +1,11 @@
+from moviepy.editor import *
+
+video = VideoFileClip("TestVideo.mp4").subclip(2,6)
+
+# Make the text. Many more options are available.
+#txt_clip = ( TextClip("My Holidays 2013",fontsize=70,color='white')
+#             .set_position('center')
+#             .set_duration(10) )
+
+#result = CompositeVideoClip([video, txt_clip]) # Overlay text on video
+#result.write_videofile("myHolidays_edited.webm",fps=25) # Many options.
--- trekantsFunktioner.py
+++ trekantsFunktioner.py
@@ -3,22 +3,22 @@ import math
 import pygame
 
 
-def findTrekantOmkreds(): #Made by Yusuf, Hadi
+def findTrekantOmkreds(): #Lead: Yusuf, team: Hadi
     a + b + c
     return [a,b,c]
 
-def trekantSider(x1, x2, x3, y1, y2, y3): #Made by Bjørn, Rasmus E, Nick
+def trekantSider(x1, x2, x3, y1, y2, y3): #Closed. Lead: Bjørn, Rasmus E, Nick
     a = math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
     b = math.sqrt((x3 - x2)**2 + (y3 - y2)**2)
     c = math.sqrt((x3 - x1)**2 + (y3 - y1)**2)
     return [a, b, c]
 
-def findTrekantAreal(sider, vinkler): #Made by Jakob, Rasmus W
+def findTrekantAreal(sider, vinkler): #Lead: Rasmus W, team: Jakob
     # sider = findTrekantSidelængde, vinkler = findTrekantVinkler
     areal = (1 / 2) * sider[0] * sider[1] * math.sin(math.radians(vinkler[2]))
     return areal
 
-def findTrekantVinkler(a,b,c): #Made by Said, Omar
+def findTrekantVinkler(a,b,c): #Lead: Said, team: Omar
     vinkelSum = 180
     taellerA = (b ** 2 + c ** 2 - a ** 2)
     naevnerA = (2 * b * c)
@@ -34,7 +34,7 @@ def findTrekantVinkler(a,b,c): #Made by Said, Omar
 
     return (A, B, C)
 
-def findTrekantOmskrevneCirkelsRadius(a,b,c): #Made by Gwion, Frederik L
+def findTrekantOmskrevneCirkelsRadius(a,b,c): #Lead: Gwion, team: Frederik L, Frederik H
     s = (a + b + c) / 2
     aAT = math.sqrt(s * (s - a) * (s - b) * (s - c))
     rAC1 = (a * b * c) / (aAT * 4)
@@ -43,24 +43,21 @@ def findTrekantOmskrevneCirkelsRadius(a,b,c): #Made by Gwion, Frederik L
     print('Radius Af Din Cirkel Er: ', rAC1)
     print('---')
 
-def findTrekantOmskrevneCirkelsKordinater(): #Made by Oliver, Mads, Liam
+def findTrekantOmskrevneCirkelsKordinater(): #Lead: Magnus, team: Christoffer, Liam
     return 0
 
-def findTrekantIndskrevneCirkelsRadius(): #Made by Michael, Alexander
+def findTrekantIndskrevneCirkelsRadius(): #Lead: Michael, team: Alexander
     return 0
 
-def findTrekantIndskrevneCirkelsKordinater(): #Made by Allan, Gabriel, Daniel
+def findTrekantIndskrevneCirkelsKordinater(): #Lead: Gabriel, team: Allan, Daniel
     return 0
 
-def findTrekantVinkelHalveringsLinjer(): #Made by
-    return 0
-
-def tegnTrekant(): #Made by Yusuf, Hadi
+def tegnTrekant(): #Lead: Yusuf, team: Hadi
     while True:
         mitVindue = pygame.display.set_mode((1000, 1000))
         mitVindue.fill((255, 255, 255))
         pygame.display.update()
         return [pygame.draw.polygon(mitVindue, (0, 0, 0), ((x_1, y_1),(x_2,y_2),(x_3,y_3)), 0)]
 
-def skrivResultater(): #Made by Gwion, Frederik L
+def skrivResultater(): #Lead: Gwion, team: Frederik L, Frederik H
     return 0
